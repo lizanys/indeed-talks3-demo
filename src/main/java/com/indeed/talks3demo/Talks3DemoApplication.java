6307efe20c75db351793526bdfb1bcf859156949
Liza Ng <lng@pivotal.io>
2019-05-22 15:32:34 +0900

    Initial commit

---
+++ src/main/java/com/indeed/talks3demo/Talks3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.indeed.talks3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talks3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Talks3DemoApplication.class, args);
+    }
+
+}
