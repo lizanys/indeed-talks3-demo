d9babc1b47c381d1cf3ffec6aa060ed27c2b9922
Liza Ng <lng@pivotal.io>
2019-05-22 15:33:18 +0900

    Add hello endpoint

---
+++ src/main/java/com/indeed/talks3demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.indeed.talks3demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+}
--- src/test/java/com/indeed/talks3demo/Talks3DemoApplicationTests.java
+++ src/test/java/com/indeed/talks3demo/Talks3DemoApplicationTests.java
@@ -2,15 +2,42 @@ package com.indeed.talks3demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Talks3DemoApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void helloEndpoint_returnsHelloWorldResponse() {
+        RequestEntity<Void> request = RequestEntity
+                .get(URI.create("http://localhost:" + port + "/hello"))
+                .build();
+
+        ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo("Hello world");
+    }
 }
